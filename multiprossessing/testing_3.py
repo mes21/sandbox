aa3f76634780bb56b6ac3ba5f745f4f0c9fe77e6
Tobias <tandorfe@gmail.com>
2023-03-27 00:24:49 +0200

    adex

--- multiprossessing/testing_3.py
+++ multiprossessing/testing_3.py
@@ -6,19 +6,21 @@ class A(object):
     def __init__(self, *args, **kwargs):
         # do other stuff
         pass
+        self.abc = ["a", "b", "c"]
+        self.processes = []
 
     def do_something(self, i):
         sleep(0.2)
-        print("int:", i)
+        print("int:", i, self.abc)
 
     def run(self):
-        processes = []
 
-        for i in ["a", "b", "c"]:
+        for i in self.abc:
             p = mp.Process(target=self.do_something, args=(i,))
-            processes.append(p)
+            p.start()
+            self.processes.append(p)
 
-        [x.start() for x in processes]
+        # [x.start() for x in self.processes]
 
 
 if __name__ == "__main__":
